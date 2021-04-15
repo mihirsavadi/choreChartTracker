28747a83fff83b79f80f8b5132b144cb9162bddb
Mihir Savadi <mihirsavadi1@vt.edu>
2021-04-15 16:37:19 -0400

    bricked seeed xiao by using std libraries esp vector string and algos. when removed constructor from main, after ruploading to board in dfu mode via stock arduino ide code works again. will rework everything to make it simpler

--- software/choreChartTracker/src/main.cpp
+++ software/choreChartTracker/src/main.cpp
@@ -1,3 +1,96 @@
+// #include <Arduino.h>
+// #include <U8g2lib.h>
+
+// #include "choreChartTracker.hpp"
+// // #include <choreChartTracker.hpp>
+
+// //macros to store number of chores and chore doers, which is fixed based on hardware
+// #define CHORE_QTY 4
+// #define DOER_QTY 4
+
+// Adafruit_VL53L0X sensor0 = Adafruit_VL53L0X();
+// VL53L0X_RangingMeasurementData_t measure0;
+
+// U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+// void setup() {
+//   Serial.begin(9600);
+//   Serial.println("poop");
+
+//   u8g2.begin();
+
+//   tofUnit tof_0; 
+//   tof_0.choreName = "dishes";
+//   tof_0.xShutPort = 0x30;
+//   tofUnit tof_1;
+//   tof_1.choreName = "emptyDishWasher";
+//   tof_1.xShutPort = 0x31;
+//   tofUnit tof_2;
+//   tof_2.choreName = "trash";
+//   tof_2.xShutPort = 0x32;
+//   tofUnit tof_3;
+//   tof_3.choreName = "recycle";
+//   tof_3.xShutPort = 0x33;
+//   std::vector<tofUnit> ToFarray;
+//   ToFarray.push_back(tof_0);
+//   ToFarray.push_back(tof_1);
+//   ToFarray.push_back(tof_2);
+//   ToFarray.push_back(tof_3);
+
+//   choreDoer doer_0;
+//   doer_0.name = "Lucas";
+//   doer_0.lowerBound = 1;
+//   doer_0.upperBound = 2;
+//   choreDoer doer_1;
+//   doer_1.name = "Mihir";
+//   doer_1.lowerBound = 3;
+//   doer_1.upperBound = 4;
+//   choreDoer doer_2;
+//   doer_2.name = "Nathan";
+//   doer_2.lowerBound = 5;
+//   doer_2.upperBound = 6;
+//   choreDoer doer_3;
+//   doer_3.name = "Vignesh";
+//   doer_3.lowerBound = 7;
+//   doer_3.upperBound = 8;
+//   std::vector<choreDoer> choreDoers;
+//   choreDoers.push_back(doer_0);
+//   choreDoers.push_back(doer_1);
+//   choreDoers.push_back(doer_2);
+//   choreDoers.push_back(doer_3);
+
+//   choreChartTracker tracker = choreChartTracker(ToFarray, choreDoers);
+
+//   delay(5000);
+
+//   // u8g2.clearBuffer();
+
+//   // bool trackerErrorFlag;
+//   // std::string trackerErrorMessage;
+//   // tracker.getError(trackerErrorFlag, trackerErrorMessage);
+//   // u8g2.drawStr(0, 50, "error flag: " + (trackerErrorFlag)?"true":"false" );
+//   // if (trackerErrorFlag)
+//   // {
+//   //   u8g2.drawStr(0, 50, trackerErrorMessage.c_str());
+//   // }
+
+//   // u8g2.sendBuffer();
+
+//   pinMode(13, OUTPUT);
+//   bool trackerErrorFlag;
+//   std::string trackerErrorMessage;
+//   tracker.getError(trackerErrorFlag, trackerErrorMessage);
+//   u8g2.drawStr(0, 50, "error flag: " + (trackerErrorFlag)?"true":"false" );
+//   if (trackerErrorFlag)
+//   {
+//    digitalWrite(13, HIGH);
+//   }
+// }
+
+// void loop() {
+
+// }
+
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <Wire.h>
@@ -41,7 +134,7 @@ void setup() {
   choreDoers.push_back(uno);
   choreDoers.push_back(dos);
 
-  choreChartTracker tracker = choreChartTracker(ToFarray, choreDoers);
+  // choreChartTracker tracker = choreChartTracker(ToFarray, choreDoers);
 
   pinMode(0, OUTPUT);
   pinMode(1, OUTPUT);
