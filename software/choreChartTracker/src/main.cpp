f749834be4aabc6a07b643167aee3742cfdd3b45
Mihir Savadi <mihirsavadi1@vt.edu>
2021-04-15 01:43:42 -0400

    figured out vl53l0x sensor code and feeding to oled

--- software/choreChartTracker/src/main.cpp
+++ software/choreChartTracker/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <Wire.h>
+#include <Adafruit_VL53L0X.h>
 
 #include "choreChartTracker.hpp"
 // #include <choreChartTracker.hpp>
@@ -9,7 +10,15 @@
 #define CHORE_QTY 4
 #define DOER_QTY 4
 
+Adafruit_VL53L0X sensor0 = Adafruit_VL53L0X();
+VL53L0X_RangingMeasurementData_t measure0;
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
 void setup() {
+  Serial.begin(9600);
+
+  u8g2.begin();
+
   tofUnit one; 
   one.choreName = "wash";
   one.xShutPort = 0;
@@ -33,8 +42,46 @@ void setup() {
   choreDoers.push_back(dos);
 
   choreChartTracker tracker = choreChartTracker(ToFarray, choreDoers);
+
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+
+  digitalWrite(0, LOW);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(10);
+  digitalWrite(0, HIGH);
+  digitalWrite(1, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  delay(10);
+  digitalWrite(0, HIGH);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+
+  if(!sensor0.begin(0x30)) {
+    Serial.println(F("Failed to boot first VL53L0X"));
+    while(1);
+  }
 }
 
 void loop() {
-  
+
+  u8g2.clearBuffer();					// clear the internal memory
+  u8g2.setFont(u8g2_font_tenstamps_mf);	// choose a suitable font
+  sensor0.rangingTest(&measure0, false);
+  char str[100];
+  sprintf(str, "%d", measure0.RangeMilliMeter);
+  if(measure0.RangeStatus != 4) {     // if not out of range
+    u8g2.drawStr(0, 50, str);
+  } else {
+    u8g2.drawStr(0, 50, "Out of range");
+  }
+  u8g2.sendBuffer();
+  delay(50);
+
 }
