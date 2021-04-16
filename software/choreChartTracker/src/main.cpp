fdd1cc165589909b95164ea01f21a219a374e479
Mihir Savadi <mihirsavadi1@vt.edu>
2021-04-15 22:35:11 -0400

    changed data entry from vector to array

--- software/choreChartTracker/src/main.cpp
+++ software/choreChartTracker/src/main.cpp
@@ -20,76 +20,33 @@ void setup() {
 
   u8g2.begin();
 
-  tofUnit tof_0; 
-  tof_0.choreName = "dishes";
-  tof_0.xShutPort = 0x30;
-  tofUnit tof_1;
-  tof_1.choreName = "emptyDishWasher";
-  tof_1.xShutPort = 0x31;
-  tofUnit tof_2;
-  tof_2.choreName = "trash";
-  tof_2.xShutPort = 0x32;
-  tofUnit tof_3;
-  tof_3.choreName = "recycle";
-  tof_3.xShutPort = 0x33;
-  std::vector<tofUnit> ToFarray;
-  ToFarray.push_back(tof_0);
-  ToFarray.push_back(tof_1);
-  ToFarray.push_back(tof_2);
-  ToFarray.push_back(tof_3);
+  tofUnit ToFarray[4];
+  ToFarray[0].choreName = "dishes";
+  ToFarray[0].xShutPort = 0x30;
+  ToFarray[1].choreName = "emptyDishWasher";
+  ToFarray[1].xShutPort = 0x31;
+  ToFarray[2].choreName = "trash";
+  ToFarray[2].xShutPort = 0x32;
+  ToFarray[3].choreName = "recycle";
+  ToFarray[3].xShutPort = 0x33;
 
-  choreDoer doer_0;
-  doer_0.name = "Lucas";
-  doer_0.lowerBound = 1;
-  doer_0.upperBound = 2;
-  choreDoer doer_1;
-  doer_1.name = "Mihir";
-  doer_1.lowerBound = 3;
-  doer_1.upperBound = 4;
-  choreDoer doer_2;
-  doer_2.name = "Nathan";
-  doer_2.lowerBound = 5;
-  doer_2.upperBound = 6;
-  choreDoer doer_3;
-  doer_3.name = "Vignesh";
-  doer_3.lowerBound = 7;
-  doer_3.upperBound = 8;
-  std::vector<choreDoer> choreDoers;
-  choreDoers.push_back(doer_0);
-  choreDoers.push_back(doer_1);
-  choreDoers.push_back(doer_2);
-  choreDoers.push_back(doer_3);
-
-  // choreChartTracker tracker = choreChartTracker(ToFarray, choreDoers);
-
-  // delay(5000);
-
-  // u8g2.clearBuffer();
-
-  // bool trackerErrorFlag;
-  // std::string trackerErrorMessage;
-  // tracker.getError(trackerErrorFlag, trackerErrorMessage);
-  // u8g2.drawStr(0, 50, "error flag: " + (trackerErrorFlag)?"true":"false" );
-  // if (trackerErrorFlag)
-  // {
-  //   u8g2.drawStr(0, 50, trackerErrorMessage.c_str());
-  // }
-
-  // u8g2.sendBuffer();
-
-  // pinMode(13, OUTPUT);
-  // bool trackerErrorFlag;
-  // std::string trackerErrorMessage;
-  // tracker.getError(trackerErrorFlag, trackerErrorMessage);
-  // u8g2.drawStr(0, 50, "error flag: " + (trackerErrorFlag)?"true":"false" );
-  // if (trackerErrorFlag)
-  // {
-  //  digitalWrite(13, HIGH);
-  // }
+  choreDoer choreDoers[4];
+  choreDoers[0].name = "Lucas";
+  choreDoers[0].lowerBound = 1;
+  choreDoers[0].upperBound = 2;
+  choreDoers[1].name = "Mihir";
+  choreDoers[1].lowerBound = 3;
+  choreDoers[1].upperBound = 4;
+  choreDoers[2].name = "Nathan";
+  choreDoers[2].lowerBound = 5;
+  choreDoers[2].upperBound = 6;
+  choreDoers[3].name = "Vignesh";
+  choreDoers[3].lowerBound = 7;
+  choreDoers[3].upperBound = 8;
 
   while(1)
   {
-    
+
   }
 }
 
