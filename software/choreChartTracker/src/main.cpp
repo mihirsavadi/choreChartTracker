abe3d3f09c6c4c0002554cd6f55e72f50c5367b8
Mihir Savadi <mihirsavadi1@vt.edu>
2021-04-18 10:22:07 -0400

    cleaned up useless code

--- software/choreChartTracker/src/main.cpp
+++ software/choreChartTracker/src/main.cpp
@@ -1,13 +1,7 @@
 #include <Arduino.h>
-#include <U8g2lib.h>
 
 #include "choreChartTracker.hpp"
 #include "oledDriver.hpp"
-
-Adafruit_VL53L0X sensor0 = Adafruit_VL53L0X();
-VL53L0X_RangingMeasurementData_t measure0;
-
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 void setup() {
   oledDriver od;
