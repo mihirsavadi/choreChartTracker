5b556bedd84d5597cdbdc496b10832a7bfc52ba6
Mihir Savadi <mihirsavadi1@vt.edu>
2021-05-31 12:28:48 -0700

    copy pasted code from rev1 into rev2. Still need to modify for esp32 compatibility and restructure class architecture for wifi/bluetooth implementation

---
+++ software/choreChartTracker_rev2/src/choreChartTracker.cpp
@@ -0,0 +1,232 @@
+// Mihir Savadi 
+// implementation of choreChartTracker.hpp
+//  See the .hpp for comments and descriptions and compare side by side to this
+//   file for the best system-level view.
+
+#include "choreChartTracker.hpp"
+
+choreChartTracker::choreChartTracker(tofUnit *tofArray_in, uint8_t tofArray_size, 
+                          choreDoer *choreDoers_in, uint8_t choreDoers_size)
+{
+    if (tofArray_in != nullptr && choreDoers_in != nullptr)
+    {
+        //1. check for critical tofArray_in constraints. then assign objects.
+        for (uint8_t i = 0; i < tofArray_size; i++)
+        {
+            if (tofArray_in[i].address == 0x68 || tofArray_in[i].address == 0x3C ||
+                tofArray_in[i].address < 0x30 || tofArray_in[i].address > 0x3F)
+            {
+                this->errorPresent = true;
+                this->errorDescription.concat(String(tofArray_in[i].address) + 
+                    " is bad" + ERDELIM);
+            }
+        }
+
+        //2. check and set values for VL53L0X sensors
+        for (uint8_t i = 0; i < tofArray_size; i++)
+        {
+            pinMode(tofArray_in[i].xShutPort, OUTPUT);
+            digitalWrite(tofArray_in[i].xShutPort, LOW);
+        }
+        delay(10);
+        for (uint8_t i = 0; i < tofArray_size; i++)
+        {
+            digitalWrite(tofArray_in[i].xShutPort, HIGH);
+        }
+        delay(10);
+        for (uint8_t i = 0; i < tofArray_size; i++)
+        {
+            digitalWrite(tofArray_in[i].xShutPort, HIGH);
+            for (uint8_t j = 0; j < tofArray_size; j++)
+            {
+                if (j > i)
+                    digitalWrite(tofArray_in[j].xShutPort, LOW);
+            }
+            if (!tofArray_in[i].sensorObject.begin(tofArray_in[i].address)) {
+                this->errorPresent = true;
+                this->errorDescription.concat("ToF address " + 
+                String(tofArray_in[i].address) + " failed to initialize" + ERDELIM);
+            }
+            delay(10);
+        }
+    }
+    else
+    {
+        this->errorPresent = true;
+        this->errorDescription.concat("choreChartTracker object passed a nullpointer for ToFarray and choreDoer array" + ERDELIM);
+    }
+
+    //3. check if DS1307 is present on i2c bus
+        if (!rtc.begin())
+        {
+            this->errorPresent = true;
+            this->errorDescription.concat("Could not find RTC" + ERDELIM);
+        }
+        if (!rtc.isrunning())
+        {
+            this->errorPresent = true;
+            this->errorDescription.concat("RTC not running. Need to set time.");
+            this->autoSetRTCtime();
+            // this->setLogTime(0, 0, 0); //use this if auto doesnt suffice.
+        }
+        
+    //4. check if SD card present on SPI bus
+        //TODO!
+
+    //5. set all private variables
+    if (!this->errorPresent)
+    {
+        this->tofArray = tofArray_in;
+        this->tofArray_size = tofArray_size;
+        this->choreDoers = choreDoers_in;
+        this->choreDoers_size = choreDoers_size;
+    }
+
+    this->constructorDone = true;
+}
+
+bool const choreChartTracker::getConstructorDoneFlag()
+{
+    return this->constructorDone;
+}
+
+bool const choreChartTracker::getError(String &errorMessage)
+{
+    errorMessage = errorDescription;
+    return this->errorPresent;
+}
+
+uint16_t const choreChartTracker::getToFmillim(uint8_t sensorIndex)
+{
+    if (sensorIndex > this->tofArray_size)
+    {
+        this->errorPresent = true;
+        this->errorDescription.concat("reading requested from sensor indexbeyond what is present" + ERDELIM);
+        return 0;
+    }
+
+    uint8_t readMax = 5;
+    uint16_t readValue = 0;
+    for (uint8_t i = 0; i < readMax; i++)
+    {
+        this->tofArray[sensorIndex].sensorObject.rangingTest(
+            &this->tofArray[sensorIndex].sensorMeasureObject, 
+            false
+        );
+        // see https://documentation.help/VL53L0X-API/RangeStatusPage.html
+        // 1,2,3,4,5 are various different error messages.
+        // typical mm readouts are above 20ish anyway.
+        if (this->tofArray[sensorIndex].sensorMeasureObject.RangeStatus != 0)
+        {
+            return this->tofArray[sensorIndex].sensorMeasureObject.RangeStatus;
+        }
+        readValue = readValue + 
+                this->tofArray[sensorIndex].sensorMeasureObject.RangeMilliMeter;
+    }
+
+    return readValue/readMax;
+}
+
+VL53L0X_RangingMeasurementData_t const choreChartTracker::getAllToFData(
+                                                            uint8_t sensorIndex)
+{
+    if (sensorIndex > this->tofArray_size)
+    {
+        this->errorPresent = true;
+        this->errorDescription.concat("details requested from sensor index beyond what is present" + ERDELIM);
+        return VL53L0X_RangingMeasurementData_t();
+    }
+
+    return this->tofArray[sensorIndex].sensorMeasureObject;
+}
+
+uint8_t const choreChartTracker::tokenInWhichRow(String *doersArray, 
+                                                        String *choreNameArray)
+{
+    String doersArrayToSend[this->tofArray_size];
+    String choreNameToSend[this->tofArray_size];
+
+    for(uint8_t  i = 0; i < this->tofArray_size; i++)
+    {
+        choreNameToSend[i] = this->tofArray[i].choreName;
+        doersArrayToSend[i] = String("choreTokenInvalidPosition");
+        for (uint8_t j = 0; j < this->choreDoers_size; j++)
+        {
+            uint16_t distance = this->getToFmillim(i);
+            if (distance >= this->choreDoers[j].upperBound && 
+                    distance <= this->choreDoers[j].lowerBound)
+            {
+                doersArrayToSend[i] = this->choreDoers[j].name;
+            }
+        }
+    }
+    
+    doersArray = doersArrayToSend;
+    choreNameArray = choreNameToSend;
+
+    return this->tofArray_size;
+}
+
+void choreChartTracker::setRTCtime(uint16_t year, uint8_t month, uint8_t date, 
+                uint8_t hour, uint8_t min, uint8_t sec)
+{    
+    DateTime adjustTime(year, month, date, hour, min, sec);
+    this->rtc.adjust(adjustTime);
+}
+
+void choreChartTracker::autoSetRTCtime()
+{
+    this->rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+}
+
+void choreChartTracker::setLogTime(uint8_t hour, uint8_t min, uint8_t sec)
+{
+    this->logHour = hour;
+    this->logMin = min;
+    this->logSec = sec;
+}
+
+String const choreChartTracker::getLogTime()
+{
+    return this->logHour + String(":") + this->logMin + String(":") + this->logSec;
+}
+
+String const choreChartTracker::getCurrentTimeDate()
+{
+    DateTime now = this->rtc.now();
+
+    String currentTime(now.year() + String(":") + now.month() + String(":") + now.day() + 
+        String(":") + now.hour() + String(":") + now.minute() + String(":") + 
+        now.second());
+
+    return currentTime;
+}
+
+String const choreChartTracker::getMostRecentLog()
+{
+    return String("dummy");
+}
+
+//TODO
+String const choreChartTracker::getEntireLog()
+{
+    return String("dummy");
+}
+
+//TODO
+void choreChartTracker::logIfLogTime()
+{
+
+}
+
+bool const choreChartTracker::isLoggingIn30()
+{
+    DateTime now = rtc.now();
+
+    if (now.hour() == this->logHour && now.minute() == this->logMin)
+    {
+        return true;
+    }
+
+    return false;
+}
---
+++ software/choreChartTracker_rev2/src/choreChartTracker.hpp
@@ -0,0 +1,190 @@
+#ifndef CHORECHARTTRACER_CPP
+#define CHORECHARTTRACER_CPP
+
+/*
+Mihir Savadi 
+started 11th April 2021
+
+Class that wraps up all the essential peripherals for the functioning of the 
+choreChartTracker as intended in its most basic form. Sort of like an API.
+    - vl53L0X array of sensors (default address of 0x29, needs to be reassigned on startup)
+    - DS1307 real time clock 
+    - SD card reader.
+OLED screen control is omitted as it is not part of the systems core function. 
+Its abstracted away in another class
+
+Only one instance of this class should be created before entering infinite loop.
+I could make this a singleton class, but that adds extra semantic complexity that
+I dont really want to deal with atm.
+
+TODO: 
+    1. test all peripherals isolated first, understand their interfaces.
+    2. create constructor with all peripheral interfacing and error handling and test.
+    3. Get on other methods.
+*/
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+
+#include <Adafruit_VL53L0X.h>
+#include <RTClib.h>
+
+#define ERDELIM String(". ")
+
+/*
+    Table for assigning data to each time of flight sensor
+        Constraints:
+            - address cannot be 0x68 which is reserved for DS1307
+            - address cannot be 0x3C which is reserved for OLED SSD1306 chip
+            - address cannot be which is reserved for any other peripheral
+                that might be used.
+            - xshut ports cannot be repeated
+            - addresses cannot be repeated
+            - choreNames cannot be repeated
+            - addresses must be between 0x30 and 0x3F
+            - all address relted constraints are critical and must be checked!
+*/
+struct tofUnit {
+    uint16_t address;  //address of ToF sensor. word is 16bit.
+    uint8_t xShutPort; //arduino pin connected to sensor's xshut port
+    String choreName;  //name of assigned chore column
+    Adafruit_VL53L0X sensorObject = Adafruit_VL53L0X();
+    VL53L0X_RangingMeasurementData_t sensorMeasureObject;
+};
+
+/*
+    Table for data for each chore doer. If one the thresholds represent choreToken distance from sensor assuming sensor is placed at the top of the chore chart.
+        constraints:
+            - Upper and lower bounds of any two ore more choreDoers cannot coincide
+            - Upper and lower bounds of any choreDoer cannot surpass vl53L0X limits.
+            - Names of any chore doer cannot be repeated
+*/
+struct choreDoer {
+    String name; //name of chore doer
+    
+    //sensor bounds for doers row location in mm. 
+    // upper is closer to sensor so lower.
+    uint16_t upperBound, lowerBound;
+};
+
+/*
+    Le class
+*/
+class choreChartTracker {
+    public:
+        /*  Only constructor. Must be called before using anything else.
+            Checks all inputs meets constraints.
+            Checks DS1307 address of 0x68 is present on i2c bus.
+            Checks if SD card present on SPI bus. 
+            Current hardware only supports 4 ToF sensors so choreDoer vector
+            must only be of size=4.
+            Sets private error fields set accordingly.
+            Multiple errors are concatenated, delineated with full-stops + space
+            If no errors, initializes all peripherals. */
+        choreChartTracker(tofUnit *tofArray_in, uint8_t tofArray_size, 
+                          choreDoer *choreDoers_in, uint8_t choreDoers_size);
+        
+        /*  Getter for constructor done flag */
+        bool const getConstructorDoneFlag();
+
+        /*  Getter for errors. returns if errorflag is set or not. if not no
+            message will be present. */
+        bool const getError(String &errorMessage);
+
+        /*  Get distance reading from each sensor in mm.
+            Sets ErrorFlag and returns 0 if sensorIndex invalid.
+            takes an average if readings for about 5 readings. Set this in method.
+            If sensor has error returns error, see this
+                https://documentation.help/VL53L0X-API/RangeStatusPage.html
+            Value = 0 : Range Valid
+            Value = 1 : Sigma Fail
+            Value = 2 : Signal Fail
+            Value = 3 : Min Range Fail
+            Value = 4 : Phase Fail
+            Value = 5 : Hardware Fail
+            Sensor will ignore everything closeer than about 20ish mm to it and
+            report out as if it were 20ish mm anyway, so error values are
+            definitely distinguishable from regular mm readouts. */
+        uint16_t const getToFmillim(uint8_t sensorIndex);
+
+        /*  Get the entire VL53L0X_RangingMeasurementData_t struct to get
+            whatever further detaiils from.
+            Sets ErrorFlag and returns empty struct if sensorIndex invalid. */
+        VL53L0X_RangingMeasurementData_t const getAllToFData(uint8_t sensorIndex);
+
+        /* Get an array of the persons row each chore token is currently sitting
+           in. The array of data which this method returns information into must
+           be passed into it as a pointer. Make sure to pass it to an array of
+           undeclared size. Each element is a string = persons name assigned to
+           that row.
+           Also returns in a corresponding array the name of the chore each persons
+           toekn is in
+           Returns size of the array being passed. This is just equal to the number
+           of ToF sensors being used == tofArray_size, but including just for 
+           convenience.
+           So to illustrate:
+            doersArray == {mihir, mihir. vignesh, nathan}
+            choreName == {dishes, emptyDishwasher, trash, recycle}
+            methodReturnValue = 4
+           Note that the order of the chore names will follow exactly the order
+            of ToFUnits were passed as a tofArray_in array into the constructor. */
+        uint8_t const tokenInWhichRow(String *doersArray, String *choreNameArray);
+
+        /*  Sets DS1307 time. Shouldn't be called during normal operation outside
+            of class, but leaving as public function for flexibility. */
+        void setRTCtime(uint16_t year, uint8_t month, uint8_t date, 
+                        uint8_t hour, uint8_t min, uint8_t sec);
+
+        /*  Sets DS1307 time automatically based on date and time the arduino 
+           sketch was compiled. Shouldn't be called during normal operation outside
+           of class, but leaving as public function for flexibility. */
+        void autoSetRTCtime();
+
+        /*  Set time to log once every day */
+        void setLogTime(uint8_t hour, uint8_t min, uint8_t sec);
+
+        /*  Gets log time as string in format "Hour:Min:Sec"    */
+        String const getLogTime();
+
+        /*  Gets current time and date from rtc as string in format 
+            "YY:MM:DD:Hour:Min:Sec" */
+        String const getCurrentTimeDate();
+
+        /*  Returns the most recent line in the SD cards log directory verbatim.*/
+        String const getMostRecentLog();
+        
+        /*  Returns entire log, with each line being \n seperated. */
+        String const getEntireLog();
+
+        /*  Checks if time to log once. Must be placed in infinite while loop
+            in main function.
+            Once called, within 30 seconds of log time, appropriate field should
+            should be set.
+            At log time, store all the following into one new line in the SD
+            in the following format
+            <year>, <month>, <date>, <choreDoer1>, <chore1>, <choreDoer2>, <chore2>, ..., <choreDoerX>, <choreX> */
+        void logIfLogTime();
+
+        /*  Getter for soon-to-log flag */
+        bool const isLoggingIn30();
+
+    private:
+        bool constructorDone = false;
+
+        bool errorPresent = false;    //1 for yes 0 for no
+        String errorDescription = ""; //default if no error
+
+        tofUnit *tofArray;
+        uint8_t tofArray_size;
+        choreDoer *choreDoers;
+        uint8_t choreDoers_size;
+
+        uint8_t logHour, logMin, logSec;
+
+        bool loggingIn30;
+
+        RTC_DS1307 rtc;
+};
+
+#endif
